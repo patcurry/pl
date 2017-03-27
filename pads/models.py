449080a7cc27a97f8435a5b3d7c1224f931e1fdb
patcurry <currypat1985@gmail.com>
2017-03-26 17:22:40 -0700

    static files set up for basic pad list

--- pads/models.py
+++ pads/models.py
@@ -2,7 +2,18 @@ from django.db import models
 
 
 class Pad(models.Model):
-    name = models.CharField(max_length=12, unique=True, blank=False)
+    name = models.CharField(max_length=20, unique=True, blank=False)
+    status = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
+
+'''
+class Locker(models.Model):
+    name = models.CharField(max_length=12, unique=True, blank=True, null=True)
+    pad = models.ManyToManyField(Pad)
+    status = models.CharField(max_length=12, blank=True, null=True)
+
+    class Meta:
+        ordering = ('status')
+'''
---
+++ static/js/pads/pad_list.js
@@ -0,0 +1,1 @@
+console.log('pad list')
